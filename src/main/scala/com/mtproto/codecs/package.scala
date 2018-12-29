34f4af7a4d171b0be24187d5aacc330ca66bd6e7
Valeriy Navozenko <valeriy.navozenko@gmail.com>
2018-12-29 02:39:47 +0100

    use ascii 32

--- src/main/scala/com/mtproto/codecs/package.scala
+++ src/main/scala/com/mtproto/codecs/package.scala
@@ -1,5 +1,7 @@
 package com.mtproto
 
+import java.nio.charset.Charset
+
 import akka.util.ByteString
 import com.mtproto.messages.{Message, ReqDHParamsMessage, ReqPqMessage, ResPqMessage}
 import scodec.Attempt.Failure
@@ -9,6 +11,8 @@ import scodec.{Codec, Err}
 
 package object codecs {
 
+  val ascii: Charset = Charset.forName("US-ASCII")
+
   def bytesString: Codec[ByteString] = bytes.asByteString
 
   implicit class RichCodecByteVector(val codec: Codec[ByteVector]) extends AnyVal {
@@ -16,10 +20,12 @@ package object codecs {
       .xmap[ByteString](byteVector => ByteString(byteVector.toArray), byteString => ByteVector(byteString))
   }
 
-  def message: Codec[Message] = variableSizeBytes(int32, bytes).exmap[Message](data => {
-    def decode[T <: Message](codec: Codec[T]) = codec.decode(data.bits).map(_.value)
+  def message: Codec[Message] = variableSizeBytes(int32, string(ascii)).exmap[Message](data => {
+    val bits = ByteVector(data.getBytes).bits
 
-    int32.decode(data.bits).flatMap {
+    def decode[T <: Message](codec: Codec[T]) = codec.decode(bits).map(_.value)
+
+    int32.decode(bits).flatMap {
       case scodec.DecodeResult(value, _) => value match {
         case ReqPqMessage.classId => decode(MessageCodecs.reqPqMessageCodec)
         case ReqDHParamsMessage.classId => decode(MessageCodecs.reqDHParamsMessageCodec)
@@ -27,9 +33,9 @@ package object codecs {
       }
     }
   }, {
-    case reqPqMessage: ReqPqMessage => MessageCodecs.reqPqMessageCodec.encode(reqPqMessage).map(_.bytes)
-    case reqDHParamsMessage: ReqDHParamsMessage => MessageCodecs.reqDHParamsMessageCodec.encode(reqDHParamsMessage).map(_.bytes)
-    case resPqMessage: ResPqMessage => MessageCodecs.resPqMessageCodec.encode(resPqMessage).map(_.bytes)
+    case reqPqMessage: ReqPqMessage => MessageCodecs.reqPqMessageCodec.encode(reqPqMessage).flatMap(ascii32.decodeValue)
+    case reqDHParamsMessage: ReqDHParamsMessage => MessageCodecs.reqDHParamsMessageCodec.encode(reqDHParamsMessage).flatMap(ascii32.decodeValue)
+    case resPqMessage: ResPqMessage => MessageCodecs.resPqMessageCodec.encode(resPqMessage).flatMap(ascii32.decodeValue)
     case message => Failure(Err(s"Can't serialize message $message"))
   })
 
